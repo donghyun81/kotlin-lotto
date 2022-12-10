7155afdb01ebcda3bb400f73830423543acdfc29
윤동현 <donghyunyoon81@gmail.com>
2022-12-10 16:05:36 +0900

    test<OutputViewTest> : 필드 생성 및 setup 기능 구현
    테스트할 OutputView 타입 필드 선언
    출력값을 저장할 outputStream 필드 선언
    각 테스트에 사용할 필드 초기화
    outputView에 OutputView을 인스턴스화 해서 대입
    outputStream에 테스트 실행시 출력되는 값을 대입

--- src/test/kotlin/lotto/OutputViewTest.kt
+++ src/test/kotlin/lotto/OutputViewTest.kt
@@ -1,4 +1,19 @@
 package lotto
 
+import lotto.view.io.OutputView
+import org.junit.jupiter.api.BeforeEach
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
+
 class OutputViewTest {
+    lateinit var outputView:OutputView
+    private val outputStream:OutputStream = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun `setup`() {
+        outputView = OutputView()
+        System.setOut(PrintStream(outputStream))
+    }
+
 }
