2c7b95284d50481cf86614ce00cc475c6b84c7aa
윤동현 <donghyunyoon81@gmail.com>
2022-12-10 16:34:19 +0900

    test<OutputViewTest> : 로또 구입 개수 출력 기능 테스트

--- src/test/kotlin/lotto/OutputViewTest.kt
+++ src/test/kotlin/lotto/OutputViewTest.kt
@@ -1,7 +1,10 @@
 package lotto
 
 import lotto.view.io.OutputView
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 import java.io.PrintStream
@@ -14,6 +17,16 @@ class OutputViewTest {
     fun `setup`() {
         outputView = OutputView()
         System.setOut(PrintStream(outputStream))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "8,8개를 구매했습니다.",
+        "3,3개를 구매했습니다."
+    )
+    fun `로또 구입 개수 출력 기능 테스트`(amount:Int,outputAmount:String) {
+        outputView.purchaseCount(amount)
+        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(outputAmount)
     }
 
 }
