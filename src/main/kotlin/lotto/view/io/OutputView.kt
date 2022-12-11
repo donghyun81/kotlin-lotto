44620d14032667cca73b29b93a47ac1447b5970c
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 11:45:13 +0900

    feat<OutputView> : guideBonusNumbers 기능 구현
    보너스 번호 입력 안내를 출력하는 기능

--- src/main/kotlin/lotto/view/io/OutputView.kt
+++ src/main/kotlin/lotto/view/io/OutputView.kt
@@ -2,15 +2,15 @@ package lotto.view.io
 
 class OutputView {
 
-    fun guidePurchaseAmount(){
+    fun guidePurchaseAmount() {
         println("구입금액을 입력해 주세요.")
     }
 
-    fun purchaseCount(count:Int){
+    fun purchaseCount(count: Int) {
         println("${count}개를 구매했습니다.")
     }
 
-    fun purchaseLottoTickets(lottoTickets :List<List<Int>>) {
+    fun purchaseLottoTickets(lottoTickets: List<List<Int>>) {
         for (lottoTicket in lottoTickets) {
             println(lottoTicket)
         }
@@ -18,5 +18,9 @@ class OutputView {
 
     fun guideWinNumbers() {
         println("당첨 번호를 입력해 주세요.")
+    }
+
+    fun guideBonusNumbers() {
+        println("보너스 번호를 입력해 주세요.")
     }
 }
