2ddc3544296e7f42ce9a6e7b8862ca31efa5e9f1
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 19:34:49 +0900

    test<InputValidateTest> : 당첨 번호 입력 형식 테스트

--- src/test/kotlin/lotto/InputValidateTest.kt
+++ src/test/kotlin/lotto/InputValidateTest.kt
@@ -1,7 +1,12 @@
 package lotto
 
 import lotto.view.validate.InputValidate
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputValidateTest {
     lateinit var inputValidate: InputValidate
@@ -10,4 +15,13 @@ class InputValidateTest {
     fun setup() {
         inputValidate = InputValidate()
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,4,5,6","1,56,12,34,55,0","1,2,3,4,5,f","1,,,,,3,4,,,5,,,5,,,6"])
+    fun `당첨 번호 입력 형식 테스트` (input:String){
+        assertThrows<IllegalArgumentException>() {
+            inputValidate.winningNumbersForm(input)
+        }
+    }
+
 }
