79efcea97739106baf63f79772feb598f5cda33e
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 15:54:05 +0900

    feat<LottoTickets> : getLottoTickets 기능 구현
    구매한 로또의 개수만큼 로또를 만들어 반환하는 기능

--- src/main/kotlin/lotto/model/LottoTickets.kt
+++ src/main/kotlin/lotto/model/LottoTickets.kt
@@ -1,7 +1,17 @@
 package lotto.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoTickets {
 
-    fun count(price:Int) = price.div(1000)
+    fun count(price: Int) = price.div(1000)
+
+    fun getLottoTickets(count: Int): List<Lotto> {
+        val lottoTickets = mutableListOf<Lotto>()
+        repeat(count) {
+            lottoTickets.add(Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)))
+        }
+        return lottoTickets
+    }
 
 }
--- src/test/kotlin/lotto/LottoTicketsTest.kt
+++ src/test/kotlin/lotto/LottoTicketsTest.kt
@@ -1,8 +1,12 @@
 package lotto
 
+import lotto.model.Lotto
 import lotto.model.LottoTickets
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.RepetitionInfo
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -10,7 +14,7 @@ class LottoTicketsTest {
     lateinit var lottoTickets: LottoTickets
 
     @BeforeEach
-    fun setup(){
+    fun setup() {
         lottoTickets = LottoTickets()
     }
 
@@ -20,8 +24,7 @@ class LottoTicketsTest {
         "0,0",
         "33000,33",
     )
-    fun `구매한 로또 티켓의 개수를 구하는 기능 테스트`(price:Int,count:Int){
-      Assertions.assertThat(lottoTickets.count(price)).isEqualTo(count)
+    fun `구매한 로또 티켓의 개수를 구하는 기능 테스트`(price: Int, count: Int) {
+        Assertions.assertThat(lottoTickets.count(price)).isEqualTo(count)
     }
-
 }
