31f93a43cd533b813737442619ad1a936a6a0165
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 15:18:37 +0900

    test<LottoTest> : 로또의 번호를 출력하는 기능 테스트

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,7 +4,6 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
-
     fun matches(win: Lotto) = numbers.intersect(win.numbers.toSet()).count()
 
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -13,7 +13,7 @@ class LottoTest {
 
     @BeforeEach
     fun setup(){
-        lotto = Lotto(listOf(1, 2, 3, 4, 5, 6,))
+        lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
     }
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
@@ -33,5 +33,10 @@ class LottoTest {
     @Test
     fun `당첨 번호와 몇개가 일치하는지 구하는 기능 테스트`() {
         Assertions.assertThat(lotto.matches(Lotto(listOf(1,2,3,4,5,8)))).isEqualTo(5)
+    }
+
+    @Test
+    fun `로또의 번호를 출력하는 기능 테스트`(){
+        Assertions.assertThat(lotto.numbers()).isEqualTo(listOf(1,2,3,4,5,6))
     }
 }
