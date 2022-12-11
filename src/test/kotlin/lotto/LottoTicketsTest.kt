4fbd93d2835e6d65f73836ae2c5c51b82e250f0f
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 15:05:27 +0900

    test<LottoTickets> : 로또 티켓들의 개수를 구하는 기능 테스트 구현

--- src/test/kotlin/lotto/LottoTicketsTest.kt
+++ src/test/kotlin/lotto/LottoTicketsTest.kt
@@ -1,7 +1,10 @@
 package lotto
 
 import lotto.model.LottoTickets
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoTicketsTest {
     lateinit var lottoTickets: LottoTickets
@@ -9,6 +12,16 @@ class LottoTicketsTest {
     @BeforeEach
     fun setup(){
         lottoTickets = LottoTickets()
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "3000,3",
+        "0,0",
+        "33000,33",
+    )
+    fun `구매한 로또 티켓의 개수를 구하는 기능 테스트`(price:Int,count:Int){
+      Assertions.assertThat(lottoTickets.count(price)).isEqualTo(count)
     }
 
 }
