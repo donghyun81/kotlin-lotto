6be62c0ba6b6f8644399b2f17b1e941c93c6a1f9
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 15:24:08 +0900

    feat<Lotto> : numbers 기능 구현
    매개변수로 받은 numbers를 반환하는 기능

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,6 +4,8 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
+
+    fun numbers() = numbers
     fun matches(win: Lotto) = numbers.intersect(win.numbers.toSet()).count()
 
 }
