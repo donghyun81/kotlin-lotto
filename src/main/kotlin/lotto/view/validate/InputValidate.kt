d391484b2fc20c5e9bb5f58867ca7a2111738270
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 19:37:08 +0900

    feat<InputValidate> : winningNumbersForm 기능 구현

--- src/main/kotlin/lotto/view/validate/InputValidate.kt
+++ src/main/kotlin/lotto/view/validate/InputValidate.kt
@@ -1,4 +1,18 @@
 package lotto.view.validate
 
 class InputValidate {
+
+    fun winningNumbersForm(input: String) {
+        val winningNumbers = input.split(",")
+        for (winningNumber in winningNumbers) {
+            require(WINNING_NUMBER_FORM.toRegex().matches(winningNumber))
+        }
+        require(winningNumbers.size == 6)
+        require(winningNumbers.distinct().size == 6)
+    }
+
+    companion object {
+        const val WINNING_NUMBER_FORM = "^[1-45]\$"
+    }
+
 }
--- src/test/kotlin/lotto/InputValidateTest.kt
+++ src/test/kotlin/lotto/InputValidateTest.kt
@@ -17,7 +17,7 @@ class InputValidateTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["1,2,4,5,6","1,56,12,34,55,0","1,2,3,4,5,f","1,,,,,3,4,,,5,,,5,,,6"])
+    @ValueSource(strings = ["1,2,4,5,6","1,56,12,34,55,0","1,2,3,4,5,f","1,2,3,,4,5,6"])
     fun `당첨 번호 입력 형식 테스트` (input:String){
         assertThrows<IllegalArgumentException>() {
             inputValidate.winningNumbersForm(input)
