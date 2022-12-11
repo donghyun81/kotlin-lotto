39560cc168d69fbf91661ef0aca2910d5b8f6b77
윤동현 <donghyunyoon81@gmail.com>
2022-12-11 20:03:13 +0900

    feat<InputValidate> : purchaseAmount 기능 구현
    입력한 값이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.
    최소 1000원 이상 구매하지 않으면 예외가 발생한다.
    숫자외에 문자가 입력되면 예외가 발생한다.

--- src/main/kotlin/lotto/view/validate/InputValidate.kt
+++ src/main/kotlin/lotto/view/validate/InputValidate.kt
@@ -2,6 +2,12 @@ package lotto.view.validate
 
 class InputValidate {
 
+    fun purchaseAmount(input: String) {
+        require(PURCHASE_AMOUNT_FORM.toRegex().matches(input))
+        require(input.toInt() % 1000 == 0)
+        require(input.toInt().div(1000) >= 1)
+    }
+
     fun winningNumbersForm(input: String) {
         val winningNumbers = input.split(",")
         for (winningNumber in winningNumbers) {
@@ -12,7 +18,9 @@ class InputValidate {
     }
 
     companion object {
+        const val PURCHASE_AMOUNT_FORM = "^[0-9]*\$"
         const val WINNING_NUMBER_FORM = "^[1-45]\$"
+
     }
 
 }
