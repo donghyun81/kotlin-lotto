65f569e34220c56cd70ff1c808021fc63ee3f750
윤동현 <donghyunyoon81@gmail.com>
2022-12-12 12:05:34 +0900

    test<StatisticTest> : 당첨 금액을 구하는 기능 테스트
    당첨 정보에 따라 당첨 금액을 반환 하는 기능 테스트

--- src/main/kotlin/lotto/model/Statistic.kt
+++ src/main/kotlin/lotto/model/Statistic.kt
@@ -17,4 +17,5 @@ class Statistic(
         return lottoStatistic
     }
 
+
 }
--- src/test/kotlin/lotto/StatisticTest.kt
+++ src/test/kotlin/lotto/StatisticTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class StatisticTest {
     lateinit var statistic: Statistic
@@ -32,6 +34,17 @@ class StatisticTest {
             { assertEquals(statistic.winning()[Pair(5, false)],2)},
             { assertEquals(statistic.winning()[Pair(5, true)],1)},
             { assertEquals(statistic.winning()[Pair(6, false)],1)},
+        )
+    }
+
+    @Test
+    fun `당첨 금액을 구하는 기능 테스트`(){
+        assertAll(
+            { assertEquals(statistic.getRevenue(Pair(4,false)),50_000)} ,
+            { assertEquals(statistic.getRevenue(Pair(4,true)),50_000)} ,
+            { assertEquals(statistic.getRevenue(Pair(5,false)),1_500_000)} ,
+            { assertEquals(statistic.getRevenue(Pair(5,true)),30_000_000)} ,
+            { assertEquals(statistic.getRevenue(Pair(6,false)),2_000_000_000)} ,
         )
     }
 }
