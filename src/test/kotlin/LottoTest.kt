5b96961b56034e7dffa10a39eab680f1f502e61b
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-08 16:22:23 +0900

    feat: 로또 넘버가 중복된 숫자를 포함하고 6개가 아닐 경우 예외 처리

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -6,4 +6,9 @@ class LottoTest {
     fun `로또 넘버가 6개가 아닐 경우 예외 처리`() {
         assertThatThrownBy { Lotto(1, 2, 3, 4, 5, 6, 7) }.isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @Test
+    fun `로또 넘버가 중복된 숫자를 포함하고 6개가 아닐 경우 예외 처리`() {
+        assertThatThrownBy { Lotto(1, 2, 3, 5, 5, 6, 6) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
