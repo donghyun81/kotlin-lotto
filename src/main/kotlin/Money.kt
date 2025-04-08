0abec2855cb5355ae49e73f77e42570afa95d69d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-08 15:46:23 +0900

    feat: 돈이 음수일 경우 예외 처리

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -1,5 +1,9 @@
 @JvmInline
 value class Money(private val value: Int) {
+    init {
+        require(value >= 0)
+    }
+
     fun purchase(price: Int): Money {
         require(value >= price)
         return Money(value - price)
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,15 +1,12 @@
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
-@JvmInline
-value class Money(private val value: Int) {
-    fun purchase(price: Int): Money {
-        require(value >= price)
-        return Money(value - price)
-    }
-}
-
 class MoneyTest {
+    @Test
+    fun `돈이 음수일 경우 예외 처리`() {
+        assertThatThrownBy { Money(-1) }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
     @Test
     fun `구입하는 금액이 돈보다 높으면 예외 처리`() {
         val money = Money(2000)
