422dee1a7de921ee576eefa3e867b2a0e319eb3c
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-14 15:24:44 +0900

    debug: 상속 수정

--- src/main/kotlin/domain/exception/MoneyException.kt
+++ src/main/kotlin/domain/exception/MoneyException.kt
@@ -1,7 +1,7 @@
 package domain.exception
 
 sealed class MoneyException : IllegalArgumentException() {
-    class InvalidPurchaseException(val price: Int, val money: Int) : LottoException()
+    class InvalidPurchaseException(val price: Int, val money: Int) : MoneyException()
 
-    class InvalidInitMoneyException(val minValue: Int) : LottoException()
+    class InvalidInitMoneyException(val minValue: Int) : MoneyException()
 }
